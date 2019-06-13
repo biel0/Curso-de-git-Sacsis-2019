1708b37883971dff0a9b5bb4dcb0eb369f599bee
biel0 <gabriel.gcfd@gmail.com>
2019-06-13 19:57:24 -0300

    Função Subtração

--- projeto.c
+++ projeto.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 
 int soma(int a, int b){
-	int d = int a + int b;
-	return d;
+	return a + b;
+}
+
+int subtracao(int a, int b){
+	return a - b;
 }
 
 int main(){
-	int a, b, c;
+	int a, b, c, d;
 	printf ("Digite 1º: \n");
 	scanf ("%d", &a);
 	printf ("Digite 2º: \n");
 	scanf ("%d", &b);
 	c = soma (a,b);
-	printf("Soma: %d", c);
+	d = subtracao (a,b);
+	printf("Soma: %d\n", c);
+	printf("Subtracao: %d\n", d);
 	return 0;
 	
 }
