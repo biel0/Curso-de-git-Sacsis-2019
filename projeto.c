6176f1d9c03645eb731aada491f43cdbcac79227
biel0 <gabriel.gcfd@gmail.com>
2019-06-13 20:02:11 -0300

    Função Multiplicação

--- projeto.c
+++ projeto.c
@@ -9,16 +9,22 @@ int subtracao(int a, int b){
 	return a - b;
 }
 
+int multiplicacao(int a, int b){
+	return a * b;
+}
+
 int main(){
-	int a, b, c, d;
+	int a, b, c, d, e;
 	printf ("Digite 1º: \n");
 	scanf ("%d", &a);
 	printf ("Digite 2º: \n");
 	scanf ("%d", &b);
 	c = soma (a,b);
 	d = subtracao (a,b);
+	e = multiplicacao (a,b);
 	printf("Soma: %d\n", c);
 	printf("Subtracao: %d\n", d);
+	printf("Multiplicacao: %d\n", e);
 	return 0;
 	
 }
